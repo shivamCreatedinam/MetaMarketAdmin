c57afdf4a0f40e2ce6a19f88cbf2f1284569ca7e
Vijay Amule <scsolutions.vijayamule@gmail.com>
2024-07-22 18:42:54 +0530

    User Profile Update & email change, mobile change

--- app/Listeners/EmailOTPListener.php
+++ app/Listeners/EmailOTPListener.php
@@ -3,8 +3,13 @@
 namespace App\Listeners;
 
 use App\Events\EmailOTPEvent;
+use App\Mail\SendOTPMail;
+use App\Models\VerificationCodes;
+use Exception;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
 class EmailOTPListener
 {
@@ -20,6 +25,21 @@ class EmailOTPListener
      */
     public function handle(EmailOTPEvent $event): void
     {
-        //
+        try {
+            Log::channel("api_auth")->info('EmailOTP Listener Triggered');
+            $verificationCode = VerificationCodes::where(["user_id" => $event->user->uuid])->first();
+            Log::channel("api_auth")->info('Verification Code fetched', ['verificationCode' => $verificationCode]);
+            $data = [
+                "name" => $event->user->name,
+                "mobile_otp" => $verificationCode->mobile_otp ?? null,
+                "email_otp" => $verificationCode->email_otp ?? null,
+                "expire_at" => date('d M Y h:i:s A', strtotime($verificationCode->expire_at)),
+            ];
+
+            Mail::to($event->user->email)->send(new SendOTPMail($data));
+            Log::channel("api_auth")->info('OTP Email Sent', ['email' => $event->user->email]);
+        } catch (Exception $e) {
+            Log::channel("api_auth")->error("Error : ".$e->getMessage());
+        }
     }
 }
